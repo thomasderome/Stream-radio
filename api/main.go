9047e9ed7aa17413b0ffec779e57b0c6fed438c5
thomasderome <thomas.derome@epitech.eu>
2026-09-07 15:24:08 +0200

    [~] Load error handler & register all route

--- api/main.go
+++ api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"radio_stream/middleware"
+	"radio_stream/routes"
 	"radio_stream/services"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +12,9 @@ func main() {
 	services.InitServices()
 
 	router := gin.Default()
+	router.Use(middleware.ErrorHandler())
+	routes.Register_all_route(router)
+
 	err := router.Run(":3000")
 	if err != nil {
 		panic(err)
