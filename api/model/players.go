0c740422bf3204bea1725b1cce25a01552f87087
thomasderome <thomas.derome@epitech.eu>
2026-09-07 15:45:15 +0200

    [~] Bind json body [+] Set volume

--- api/model/players.go
+++ api/model/players.go
@@ -3,3 +3,7 @@ package model
 type SetPlayStateRequest struct {
 	PlayState bool `json:"play_state"`
 }
+
+type SetVolumeRequest struct {
+	Volume float64 `json:"volume"`
+}
--- api/routes/players/players.go
+++ api/routes/players/players.go
@@ -3,6 +3,7 @@ package players
 import (
 	"net/http"
 	"radio_stream/model"
+	"radio_stream/utils"
 
 	"github.com/gin-gonic/gin"
 
@@ -13,13 +14,16 @@ func Register(engine *gin.Engine) {
 	r := engine.Group("/players")
 
 	r.PUT("/set_play_state", setPlayState)
+	r.PUT("/set_volume", setVolume)
+
+	r.GET("/get_play_data", getPlayState)
 }
 
 func setPlayState(c *gin.Context) {
 	var body model.SetPlayStateRequest
 
-	if err := c.ShouldBindJSON(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := utils.BodyBinder(c, &body); err != nil {
+		return
 	}
 
 	if body.PlayState {
@@ -37,4 +41,31 @@ func setPlayState(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 	c.JSON(http.StatusOK, gin.H{"play_state": body.PlayState})
+}
+
+func setVolume(c *gin.Context) {
+	var body model.SetVolumeRequest
+	if err := utils.BodyBinder(c, &body); err != nil {
+		return
+	}
+
+	volume, err := s.PlayersService.SetVolume(body.Volume)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+	}
+
+	if body.Volume > 1 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Volume must be >= 1"})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"volume": volume})
+}
+
+func getPlayState(c *gin.Context) {
+	data, err := s.GetStationData()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, data)
 }
