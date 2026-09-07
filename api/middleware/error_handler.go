efd79daaa2211dc1cdaf770bc5e0322b5ecc1e55
thomasderome <thomas.derome@epitech.eu>
2026-09-07 15:23:19 +0200

    [+] Mid error handler

---
+++ api/middleware/error_handler.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		if len(c.Errors) > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": c.Errors.String(),
+			})
+		}
+	}
+}
