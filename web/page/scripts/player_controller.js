b0bccd82632a98d6c2e08de0803d7155d31bcfcd
thomasderome <thomas.derome@epitech.eu>
2026-09-12 17:57:18 +0200

    [+] Sync volume slider with API

--- web/page/scripts/player_controller.js
+++ web/page/scripts/player_controller.js
@@ -1,10 +1,17 @@
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 class Player_controller {
     constructor() {
         this.base_url = "http://127.0.0.1:3000"
         this.state_play = false;
 
         this.buttonPlay = document.getElementById("button_play");
-        this.voluleSlider = document.getElementById("volume");
+
+        this.volumeSlider = document.getElementById("volume");
+        this.volumeLevel = document.getElementById("volume_value");
+        this.allowUpdateVolume = true;
+        this.volumeSlider.addEventListener("input", () => this.updateVolumeNumber());
+
         this.imgRadioRead = document.getElementById("name_radio_read");
         this.nameRadioRead = document.getElementById("img_radio_read");
 
@@ -17,13 +24,44 @@ class Player_controller {
             .then((resp) => {
                 this.updateGui(resp)
             })
+            .catch((err) => {
+                alert("Une erreur c'est produite !");
+                console.error(err);
+            })
     }
 
     updateGui(data) {
         //buttonPlay = resp.is_playing
-        this.voluleSlider.value = data.volume;
+        this.volumeSlider.value = data.volume;
+        this.volumeLevel.textContent = `${this.volumeSlider.value}%`;
     }
 
+    updateVolumeNumber() {
+        this.volumeLevel.textContent = `${this.volumeSlider.value}%`;
+
+        if (this.allowUpdateVolume) {
+            this.allowUpdateVolume = false
+
+            setTimeout(() => {
+                fetch(this.base_url+"/player/set_volume",  {
+                    body: JSON.stringify({
+                        volume: parseFloat(this.volumeSlider.value) / 100,
+                    }),
+                    method: "PUT"
+                })
+                    .then((resp) => resp.json())
+                    .then((resp) => {
+                        this.volumeSlider.value = resp.volume;
+                        this.volumeLevel.textContent = `${this.volumeSlider.value}%`;
+                    })
+                    .catch((err) => {
+                        alert("Une erreur c'est produite !");
+                        console.error(err);
+                    })
+                this.allowUpdateVolume = true
+            }, 200);
+        }
+    }
 }
 
 function setup() {
